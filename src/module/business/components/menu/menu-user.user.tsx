988f56340e85c31e121c546ad65f93a962180333
igilfu <igilfu@gmail.com>
2020-05-12 10:24:54 +0300

    first commit

--- src/module/business/components/menu/menu-user.user.tsx
+++ src/module/business/components/menu/menu-user.user.tsx
@@ -1,110 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import menuUserStyle from './menu-user.module.scss';
-import Dropdown from './components/dropdown/dropdown.menu';
-import { NavLink } from 'react-router-dom';
-import { FaRegCalendarAlt } from 'react-icons/fa';
-import { BsGraphUp } from 'react-icons/bs';
-import { MdSettings, MdKeyboardArrowLeft, MdPeople, MdDashboard } from 'react-icons/md';
+import React, { useState, useEffect } from "react";
+import menuUserStyle from "./menu-user.module.scss";
+import Dropdown from "./components/dropdown/dropdown.menu";
+import { NavLink } from "react-router-dom";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { BsGraphUp } from "react-icons/bs";
+import {
+  MdSettings,
+  MdKeyboardArrowLeft,
+  MdPeople,
+  MdDashboard,
+} from "react-icons/md";
 
+const prop = {
+  title: "שלום, דור לוי.",
+  navlink: ["בית", "יומן תורים", "סטטיסטיקות", "הגדרות"],
+};
 
 const MenuUser = () => {
-    const [ShowDropdown, setShowDropdown] = useState<boolean>(false);
-    const [ShowMobileMenu, setShowMobileMenu] = useState<any>({ transform: "translateX(100%)" });
-    const [IsMobile, setIsMobile] = useState<any>(false);
+  const [ShowDropdown, setShowDropdown] = useState<boolean>(false);
+  const [ShowMobileMenu, setShowMobileMenu] = useState<any>({
+    transform: "translateX(100%)",
+  });
+  const [IsMobile, setIsMobile] = useState<any>(false);
 
-    // Component did mount
-    useEffect(() => {
-        resize();
-    }, []);
+  // Component did mount
+  useEffect(() => {
+    resize();
+  }, []);
 
-
-    // Check if the device witdh <= 600px
-    const resize = () => {
-        let currentHideNav = (window.innerWidth <= 600);
-        if (currentHideNav !== IsMobile) {
-            setIsMobile({ hideNav: currentHideNav });
-        }
+  // Check if the device witdh <= 600px
+  const resize = () => {
+    let currentHideNav = window.innerWidth <= 600;
+    if (currentHideNav !== IsMobile) {
+      setIsMobile({ hideNav: currentHideNav });
     }
+  };
 
-
-    // Menu button that show just in mobile device
-    const menuButtonForMobile = () => {
-        return (
-            <div className={menuUserStyle.ButtonForMobile}>
-                <input
-                    type="checkbox"
-                    className={menuUserStyle.Checkbox}
-                    id="navi-toggle"
-                    onClick={() => {
-                        if (ShowMobileMenu.transform === "translateX(100%)")
-                            setShowMobileMenu({ transform: "translateX(0)" })
-                        else
-                            setShowMobileMenu({ transform: "translateX(100%)" })
-
-                    }}
-                />
-                <label htmlFor="navi-toggle" className={menuUserStyle.Navigation_button} >
-                    <span className={menuUserStyle.Icon}>&nbsp;</span>
-                </label>
-            </div>
-        )
-    }
-
-
+  // Menu button that show just in mobile device
+  const menuButtonForMobile = () => {
     return (
-        <React.Fragment>
-            {IsMobile && menuButtonForMobile()}
+      <div className={menuUserStyle.ButtonForMobile}>
+        <input
+          type="checkbox"
+          className={menuUserStyle.Checkbox}
+          id="navi-toggle"
+          onClick={() => {
+            if (ShowMobileMenu.transform === "translateX(100%)")
+              setShowMobileMenu({ transform: "translateX(0)" });
+            else setShowMobileMenu({ transform: "translateX(100%)" });
+          }}
+        />
+        <label
+          htmlFor="navi-toggle"
+          className={menuUserStyle.Navigation_button}
+        >
+          <span className={menuUserStyle.Icon}>&nbsp;</span>
+        </label>
+      </div>
+    );
+  };
 
-            <div className={menuUserStyle.Menu} style={IsMobile ? ShowMobileMenu : {}}>
-                <p className={menuUserStyle.Logo}> Queue </p>
-                <div className={menuUserStyle.User}>
-                    <p>שלום, דור לוי.</p>
-                </div>
-                <hr />
-                <div className={menuUserStyle.Items}>
-                    <NavLink className={menuUserStyle.MenuItem} activeClassName={menuUserStyle.Current} exact to='/user'>
-                        <div className={menuUserStyle.Text}>
-                            <MdDashboard />
-                            <span>בית</span>
-                        </div>
-                    </NavLink>
+  return (
+    <React.Fragment>
+      {IsMobile && menuButtonForMobile()}
 
-                    <NavLink className={menuUserStyle.MenuItem} activeClassName={menuUserStyle.Current} to='/user/calander'>
-                        <div className={menuUserStyle.Text}>
-                            <FaRegCalendarAlt />
-                            <span> יומן תורים</span>
-                        </div>
-                    </NavLink>
+      <div
+        className={menuUserStyle.Menu}
+        style={IsMobile ? ShowMobileMenu : {}}
+      >
+        <p className={menuUserStyle.Logo}> Queue </p>
+        <div className={menuUserStyle.User}>
+          <p>{prop.title}</p>
+        </div>
+        <hr />
+        <div className={menuUserStyle.Items}>
+          {prop.navlink.map((nav) => (
+            <NavLink
+              className={menuUserStyle.MenuItem}
+              activeClassName={menuUserStyle.Current}
+              exact
+              to="/user"
+            >
+              <div className={menuUserStyle.Text}>
+                <MdDashboard />
+                <span>{nav}</span>
+              </div>
+            </NavLink>
+          ))}
 
-                    <div className={menuUserStyle.MenuItem + " " + menuUserStyle.Dropdown} >
-                        <div className={menuUserStyle.Text} onClick={() => { setShowDropdown(!ShowDropdown) }}>
-                            <MdPeople />
-                            <span >לקוחות</span>
-                            <MdKeyboardArrowLeft size="1.3rem" className={menuUserStyle.ArrowIcon}
-                                style={ShowDropdown ? { transform: ' rotate(-90deg)' } : {}} />
-                        </div>
-
-                        <Dropdown items={[{ title: 'כל הלקוחות', url: '/user/allcustomers' }, { title: 'לקוחות מהשבוע', url: '/user/customerweek' }]}
-                            show={ShowDropdown} />
-                    </div>
-
-                    <NavLink className={menuUserStyle.MenuItem} activeClassName={menuUserStyle.Current} to='/s'>
-                        <div className={menuUserStyle.Text}>
-                            <BsGraphUp />
-                            <span>סטטיסטיקות</span>
-                        </div>
-                    </NavLink>
-
-                    <NavLink className={menuUserStyle.MenuItem} activeClassName={menuUserStyle.Current} to='/user/settings'>
-                        <div className={menuUserStyle.Text}>
-                            <MdSettings />
-                            <span>הגדרות</span>
-                        </div>
-                    </NavLink>
-                </div>
+          <div
+            className={menuUserStyle.MenuItem + " " + menuUserStyle.Dropdown}
+          >
+            <div
+              className={menuUserStyle.Text}
+              onClick={() => {
+                setShowDropdown(!ShowDropdown);
+              }}
+            >
+              <MdPeople />
+              <span>לקוחות</span>
+              <MdKeyboardArrowLeft
+                size="1.3rem"
+                className={menuUserStyle.ArrowIcon}
+                style={ShowDropdown ? { transform: " rotate(-90deg)" } : {}}
+              />
             </div>
-        </React.Fragment>
-    )
-}
+
+            <Dropdown
+              items={[
+                { title: "כל הלקוחות", url: "/user/allcustomers" },
+                { title: "לקוחות מהשבוע", url: "/user/customerweek" },
+              ]}
+              show={ShowDropdown}
+            />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default MenuUser;
--- src/module/business/routing/routes.user.tsx
+++ src/module/business/routing/routes.user.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "../components/home/home.user";
+import home from "../components/home/home.user";
 import Customers from "../components/customers/customers.user";
 import CalanderUser from "../components/calendar/calendar.user";
 
-
 const UserRouting = () => {
-    return (
-        <Switch>
-            <Route exact path="/user" component={Home} />   
-            <Route path="/user/customers" component={Customers} />
-            <Route path="/user/calander" component={CalanderUser} />
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route exact path="/user" component={home} />
+      <Route path="/user/customers" component={Customers} />
+      <Route path="/user/calander" component={CalanderUser} />
+    </Switch>
+  );
 };
 
 export default UserRouting;
